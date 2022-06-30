3365da044e99c40bee79923939280126c734ac36
mesoma <anigbogumesoma1@gmail.com>
2022-06-29 18:37:30 -0700

    making files executable

---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ class rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """ Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """width setter
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+
