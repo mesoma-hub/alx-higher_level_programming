a7538fecf7a82917e141ce250a7bacfa7693b6f9
mesoma-hub <anigbogumesoma1@gmail.com>
2022-07-13 02:00:34 -0700

    python almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,45 +1,83 @@
 #!/usr/bin/python3
-"""
-module
-"""
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class Square"""
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ check inputs """
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """ check inputs """
-        return super().width
+        """Get/set the size of the Square."""
+        return self.width
 
     @size.setter
     def size(self, value):
-        """ check inputs """
-        Rectangle.width.fset(self, value)
-        Rectangle.height.fset(self, value)
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """ check inputs """
-        if kwargs and (args is None or len(args) == 0):
-            if 'size' in kwargs.keys():
-                aux = {'width': kwargs['size'], 'height': kwargs['size']}
-                kwargs.update(aux)
-            super().update(*args, **kwargs)
-        else:
-            args_list = list(args)
-            if len(args) > 1:
-                args_list.insert(1, args[1])
-            super().update(*args_list, **kwargs)
+        """Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """ check inputs """
-        new_dict = {'id': self.id,
-                    'size': self.height,
-                    'x': self.x,
-                    'y': self.y}
-        return new_dict
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
